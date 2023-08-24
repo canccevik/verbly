c70c14609e335ba6e7e0925ee7915e2d749801ec
Can Çevik <mazlumcancevik@gmail.com>
2023-08-24 03:06:10 +0300

    feat: add nestjs-envalid

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from '@modules/app.module'
+import { ENV, Config } from '@config/index'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
-  await app.listen(3001)
+  const config = app.get<Config>(ENV)
+
+  await app.listen(config.PORT)
 }
 bootstrap()
--- apps/api/src/modules/app.module.ts
+++ apps/api/src/modules/app.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common'
+import { EnvalidModule } from 'nestjs-envalid'
+import { validators } from '@config/index'
 
-@Module({})
+@Module({
+  imports: [
+    EnvalidModule.forRoot({
+      isGlobal: true,
+      useDotenv: true,
+      validators
+    })
+  ]
+})
 export class AppModule {}
