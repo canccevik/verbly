a8f803aa1a81fb31ecb1277bf705668836c4e01a
Can Çevik <mazlumcancevik@gmail.com>
2023-08-25 04:04:16 +0300

    feat(api): add user repository

--- apps/api/src/features/user/user.module.ts
+++ apps/api/src/features/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
+import { UserRepository } from './user.repository'
+import { MongooseModule } from '@nestjs/mongoose'
+import { User, UserSchema } from './user.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, UserRepository],
+  exports: [UserRepository]
 })
 export class UserModule {}
---
+++ apps/api/src/features/user/user.repository.ts
@@ -0,0 +1,12 @@
+import { BaseRepository } from '@core/repositories'
+import { User } from './user.schema'
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+
+@Injectable()
+export class UserRepository extends BaseRepository<User> {
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {
+    super(userModel)
+  }
+}
