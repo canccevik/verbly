39994f7b64f49daef5b2bf915acd22963b794c24
Can Çevik <mazlumcancevik@gmail.com>
2023-08-27 05:13:01 +0300

    test(api): add test for register method of auth service

---
+++ apps/api/src/features/auth/services/auth.service.test.ts
@@ -0,0 +1,78 @@
+import { Test } from '@nestjs/testing'
+import { createMock } from '@golevelup/ts-jest'
+import { AuthService } from '../services'
+import { UserRepository } from '@features/user/repositories'
+import { OTPRepository } from '../repositories'
+import { Queue } from 'bull'
+import { getQueueToken } from '@nestjs/bull'
+import { MAIL_QUEUE, VERIFICATION } from '@modules/mail/mail.constant'
+import { RegisterDto } from '../dto'
+import { UserDocument } from '@features/user/schemas'
+import otpGenerator from 'otp-generator'
+
+describe('AuthService', () => {
+  let authService: AuthService
+  let userRepository: UserRepository
+  let otpRepository: OTPRepository
+  let mailQueue: Queue
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: getQueueToken(MAIL_QUEUE), useValue: createMock<Queue>() }
+      ]
+    })
+      .useMocker(createMock)
+      .compile()
+
+    authService = module.get<AuthService>(AuthService)
+    userRepository = module.get<UserRepository>(UserRepository)
+    otpRepository = module.get<OTPRepository>(OTPRepository)
+    mailQueue = module.get<Queue>(getQueueToken(MAIL_QUEUE))
+  })
+
+  it('should auth service to be defined', () => {
+    // ASSERT
+    expect(authService).toBeDefined()
+  })
+
+  it('should user repository to be defined', () => {
+    // ASSERT
+    expect(userRepository).toBeDefined()
+  })
+
+  it('should otp repository to be defined', () => {
+    // ASSERT
+    expect(otpRepository).toBeDefined()
+  })
+
+  it('should mail queue to be defined', () => {
+    // ASSERT
+    expect(mailQueue).toBeDefined()
+  })
+
+  describe('register', () => {
+    it('should create otp and user then call mail queue', async () => {
+      // ARRANGE
+      const userMock = {
+        username: 'johndoe',
+        email: 'johndoe@gmail.com',
+        password: 'john123'
+      } as UserDocument
+
+      const otpCode = '123456'
+      jest.spyOn(otpGenerator, 'generate').mockReturnValue(otpCode)
+      jest.spyOn(userRepository, 'create').mockResolvedValue(userMock)
+
+      // ACT
+      const result = await authService.register(userMock as RegisterDto)
+
+      // ASSERT
+      expect(userRepository.create).toHaveBeenCalledWith(userMock)
+      expect(result.password).toBeUndefined()
+      expect(result).toEqual(userMock)
+      expect(mailQueue.add).toHaveBeenCalledWith(VERIFICATION, { email: userMock.email, otpCode })
+    })
+  })
+})
