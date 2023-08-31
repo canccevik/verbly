fd5026fff802bc2b9a33b14c780de5a446404b97
Can Çevik <mazlumcancevik@gmail.com>
2023-08-31 06:52:22 +0300

    feat(api): add skip self user guard decorator

--- apps/api/src/core/decorators/index.ts
+++ apps/api/src/core/decorators/index.ts
@@ -1,2 +1,3 @@
 export * from './message.decorator'
 export * from './user.decorator'
+export * from './skip-self-user-guard.decorator'
--- apps/api/src/core/guards/self-user.guard.ts
+++ apps/api/src/core/guards/self-user.guard.ts
@@ -2,15 +2,23 @@ import { UserDocument } from '@features/user/schemas'
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import { Request } from 'express'
 import { AuthenticatedGuard } from './authenticated.guard'
+import { Reflector } from '@nestjs/core'
+import { SkipSelfUserGuard } from '@core/decorators'
 
 @Injectable()
 export class SelfUserGuard extends AuthenticatedGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {
+    super()
+  }
+
   public canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>()
     const user = request.user as UserDocument
+
     const isAuthenticated = super.canActivate(context)
     const hasAccess = user.id === request.params.userId
+    const skipSelfUserGuard = this.reflector.get(SkipSelfUserGuard, context.getHandler())
 
-    return isAuthenticated && hasAccess
+    return !!skipSelfUserGuard || (isAuthenticated && hasAccess)
   }
 }
