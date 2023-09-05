fd5e250872724496e3bd1c652558f477e1c6f1c2
Can Çevik <mazlumcancevik@gmail.com>
2023-09-05 06:50:01 +0300

    build: remove packages

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -5,14 +5,40 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module'
   },
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    '@verbly/eslint-config',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off'
-  }
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-console': ['error']
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            accessibility: 'explicit',
+            overrides: {
+              constructors: 'no-public'
+            }
+          }
+        ],
+        '@typescript-eslint/explicit-function-return-type': ['error']
+      }
+    }
+  ]
 }
