4936fe9adf88b7d05b0c3c39f8ad0aac41295a7c
Can Çevik <mazlumcancevik@gmail.com>
2023-09-18 17:08:50 +0300

    feat(web): add update password skeleton

---
+++ apps/web/src/app/settings/password/skeleton.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+export default function UpdatePasswordSkeleton() {
+  return (
+    <div className="w-full flex flex-col gap-y-8">
+      <div className="flex flex-col gap-y-3">
+        <Skeleton className="w-20 h-5" />
+        <Skeleton className="w-full h-[60px]" />
+      </div>
+
+      <div className="flex flex-col gap-y-3">
+        <Skeleton className="w-20 h-5" />
+        <Skeleton className="w-full h-[60px]" />
+      </div>
+
+      <div className="flex flex-col gap-y-3">
+        <Skeleton className="w-20 h-5" />
+        <Skeleton className="w-full h-[60px]" />
+      </div>
+
+      <Skeleton className="w-full h-[60px]" />
+    </div>
+  )
+}
